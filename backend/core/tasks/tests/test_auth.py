b686ff7469ea5423a68672dd92b13c8e9be77091
Michael Costa <michaelgm.costa@gmail.com>
2026-01-28 11:44:33 -0300

    add: task tests

---
+++ backend/core/tasks/tests/test_auth.py
@@ -0,0 +1,46 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+
+
+class TaskAuthTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test", password="12345"
+        )
+
+    def test_login_returns_jwt_token(self):
+        """
+        Garante que o login retorna access e refresh tokens.
+        """
+        response = self.client.post("/api/auth/login/", {
+            "username": "test",
+            "password": "12345"
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+
+    def test_access_protected_route_without_token(self):
+        """
+        Garante que rotas protegidas bloqueiem acesso sem token.
+        """
+        response = self.client.get("/api/tasks/")
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_access_protected_route_with_token(self):
+        """
+        Garante acesso autorizado com JWT válido.
+        """
+        login = self.client.post("/api/auth/login/", {
+            "username": "test",
+            "password": "12345"
+        })
+
+        token = login.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+        response = self.client.get("/api/tasks/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
